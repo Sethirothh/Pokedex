d616164e1c15a0b8f48560d1bbd8e0d7bd6f1352
Jesper Gyldenløve <jespergyldenlve@gmail.com>
2022-03-03 14:47:44 +0100

    ledig-tid: pokedex first init

---
+++ pokedex/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue')
+  },
+  {
+    path: '/favorite',
+    name: 'favorite',
+    component: () => import('../views/FavoriteView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ pokedex/src/store/index.js
@@ -0,0 +1,42 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    statePokemonDataList: [],
+    stateFavoritePokemonList: []
+  },
+  actions: {
+    setPokemonData(context, payload) {
+      context.commit("setPokemonData", payload);
+    },
+    setFavoritePokemonList(context, payload) {
+      context.commit("setFavoritePokemonList", payload);
+    },
+    addFavorite(context, payload) {
+      context.commit("addFavorite", payload);
+    },
+    deleteFavorite(context, payload) {
+      context.commit("deleteFavorite", payload);
+    },
+    eraseFavoritePokemonList(context) {
+      context.commit("eraseFavoritePokemonList");
+    }
+  },
+  mutations: {
+    setPokemonData(state, list) {
+      state.statePokemonDataList = list;
+    },
+    setFavoritePokemonList(state, list) {
+      state.stateFavoritePokemonList = list;
+    },
+    addFavorite(state, name) {
+      state.stateFavoritePokemonList.push(name);
+    },
+    deleteFavorite(state, item) {
+      state.stateFavoritePokemonList.splice(item, 1);
+    },
+    eraseFavoritePokemonList(state) {
+      state.stateFavoritePokemonList = [];
+    }
+  }
+})
